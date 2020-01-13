6a6f67a61abdfaf07c54710b212768c33a1c0cca
Rustyiller <s17707@pjwstk.edu.pl>
2020-01-13 10:58:58 +0100

    added require value to stats and rank models

--- src/models/rank.js
+++ src/models/rank.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const rankSchema = mongoose.Schema({
     id: ObjectId,
-    image: { type: String },
-    tier: { type: String },
-    name: { type: String },
+    image: { type: String, required: true },
+    tier: { type: String, required: true },
+    name: { type: String, required: true },
 });
 
 const Rank = mongoose.model('Rank', rankSchema);
--- src/models/stats.js
+++ src/models/stats.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const statsSchema = mongoose.Schema({
     id: ObjectId,
-    strength: { type: Number },
-    intelligence: { type: Number },
-    fluency: { type: Number },
-    creativity: { type: Number },
+    strength: { type: Number, required: true, default: 0 },
+    intelligence: { type: Number, required: true, default: 0 },
+    fluency: { type: Number, required: true, default: 0 },
+    creativity: { type: Number, required: true, default: 0 },
 });
 
 const Stats = mongoose.model('Stats', statsSchema);
