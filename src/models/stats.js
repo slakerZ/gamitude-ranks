6b9d5a7e9fd665218025796eb3514da0fe0bb95a
Rustyiller <s17707@pjwstk.edu.pl>
2020-01-19 11:22:13 +0100

    added validations to stats values

--- src/models/stats.js
+++ src/models/stats.js
@@ -3,10 +3,46 @@ const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 const statsSchema = mongoose.Schema({
     id: ObjectId,
-    strength: { type: Number, required: true, default: 0 },
-    intelligence: { type: Number, required: true, default: 0 },
-    fluency: { type: Number, required: true, default: 0 },
-    creativity: { type: Number, required: true, default: 0 },
+    strength: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value) {
+            if (value < 0 || value > 100) {
+                throw new Error('Strength value must be between 0 and 100');
+            }
+        },
+    },
+    intelligence: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value) {
+            if (value < 0 || value > 100) {
+                throw new Error('Intelligence value must be between 0 and 100');
+            }
+        },
+    },
+    fluency: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value) {
+            if (value < 0 || value > 100) {
+                throw new Error('Fluency value must be between 0 and 100');
+            }
+        },
+    },
+    creativity: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate(value) {
+            if (value < 0 || value > 100) {
+                throw new Error('Creativity value must be between 0 and 100');
+            }
+        },
+    },
 });
 
 const Stats = mongoose.model('Stats', statsSchema);
